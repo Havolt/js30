cdce4cb93341d98e46ce4f0348a985633ba2fef2
Mark Fitzpatrick <markfitz815@gmail.com>
2018-10-25 18:02:34 +0100

    input now filters through names depending on value

--- 6. Ajax Type Ahead/scripts.js
+++ 6. Ajax Type Ahead/scripts.js
@@ -2,14 +2,43 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 
 const cities = [];
 
-fetch(endpoint)
-    .then((res) => {
-        if(res.status !== 200) {
-            console.log('Problem Occured');
-            return;
-        }
-
-        res.json().then((data) => {
-            console.log(data);
+function getData () {
+    fetch(endpoint)
+        .then((res) => {
+            if(res.status !== 200) {
+                console.log('Problem Occured');
+                return;
+            }
+            res.json().then((data) => {
+                data.map( el => {
+                    cities.push(el);
+                })
+                console.log(cities);
+            })
+        .catch((err) => {
+            console.log('Fetch Error');
         })
     })
+}
+
+function checkList(word) {
+    
+    const filterCities = cities.filter((el) => {
+        if(el.city.toUpperCase().includes(word.toUpperCase())) {
+            console.log(el.city)
+        }
+    })
+    console.log(word);
+}
+
+function eventListeners() {
+    document.querySelector('.cityInput').addEventListener('input', (e) => {
+        checkList(document.querySelector('.cityInput').value);
+    })
+}
+
+(function initApp() {
+    getData();
+    eventListeners();
+})()
+    
