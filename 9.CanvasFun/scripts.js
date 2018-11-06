d91a7a946e93fa8c6e877b4218bdaac600e7e233
Mark Fitzpatrick <markfitz815@gmail.com>
2018-11-06 19:17:46 +0000

    squares now drawn in place

--- 9.CanvasFun/scripts.js
+++ 9.CanvasFun/scripts.js
@@ -15,20 +15,27 @@ function defineCanvas(cob) {
     cob.el.style.height = cob.height;
 }
 
-function drawToCanvas() {
-
+function drawToCanvas(x, y, rgb) {
+    console.log(x, y, rgb)
+    ctx.fillStyle=`rgb(${rgb.r},${rgb.g},${rgb.b})`;
+    ctx.fillRect(x, y, 5, 5)
+    
 }
 
 function evtListeners() {
-    cnvInfo.el.addEventListener('mousedown', (evt) => {
-        cnvInfo.el.onmousemove = () => {
-            console.log(evt)
+    cnvInfo.el.addEventListener('mousedown', () => {
+        const rgb = {
+            r: Math.floor(Math.random() * 220),
+            g: Math.floor(Math.random() * 220),
+            b: Math.floor(Math.random() * 220)
+        }
+        cnvInfo.el.onmousemove = (evt) => {
+            drawToCanvas(evt.clientX, evt.clientY, rgb)
         }
     })
     cnvInfo.el.addEventListener('mouseup', (evt) => {
         cnvInfo.el.onmousemove = null;
     })
-    
 }
 
 (function initApp(){
