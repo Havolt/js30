b8edac1e3e249cae4b3978ffef736196b88d35b9
Mark Fitzpatrick <markfitz815@gmail.com>
2018-11-08 13:38:32 +0000

    init commit for multiple checks

---
+++ 10.MultipleChecks/scripts.js
@@ -0,0 +1,14 @@
+
+function makeCheckboxes () {
+    const checkText = [
+        'This is an inbox layout', 'Check one item', 'Hold down your Shift key',
+        'Check a lower item', 'Everything inbetween should also be set to checked', 
+        'Try do it without and libraries', 'Just regular JavaScript', 'Good Luck!', 
+        'Don\'t forget to tweet your results'     
+    ]
+    console.log(checkText)
+}
+
+(function initApp () {
+    makeCheckboxes();
+})()
