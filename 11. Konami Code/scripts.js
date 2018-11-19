27ad622b56266b2239e58c91410035dcc0c9a80a
Mark Fitzpatrick <markfitz815@gmail.com>
2018-11-19 14:33:54 +0000

    add event listener and check if inputs are same as konami code

--- 11. Konami Code/scripts.js
+++ 11. Konami Code/scripts.js
@@ -1,4 +1,34 @@
 
-const userInput = [];
+let userInput = [];
 
 const konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
+
+
+function keyCheck(e) {
+    let arrOkay = true;
+    if(e.keyCode !== 116) {
+        userInput.push(e.keyCode);
+    }
+    for(let i = 0; i < userInput.length; i++) {
+        if(userInput[i] != konamiCode[i]) {
+            userInput = [];
+            arrOkay = false;
+            break;
+        }
+    }
+    
+    if(arrOkay) {
+        fillCharacters();
+    }
+    
+}
+
+function fillCharacters() {
+    console.log(userInput);
+}
+
+(function initApp() {
+    document.addEventListener('keyup', (e) => {
+        keyCheck(e);
+    })
+})()
