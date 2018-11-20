a3c0893a94006e1ddbce287ee908b59c5002fcee
Mark Fitzpatrick <markfitz815@gmail.com>
2018-11-20 18:28:13 +0000

    add function to find lowest character code in comparison

--- 12.Sort Band Names/scripts.js
+++ 12.Sort Band Names/scripts.js
@@ -3,22 +3,18 @@ const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'N
     'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 
     'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
+const bandsCopy = [];
+
 function sortNames(arr) {
-    const bandCopy = [];
-
     bands.map((el, ind) => {
-        if(ind == 0) {
-            bandCopy.push(el);
+        if(ind == 0 ) {
+            bandsCopy.push(el);
         } else {
-
             let tmpEl = removeArticle(el)
-            console.log(tmpEl)
-
+            findAndPlaceEl(bandsCopy, tmpEl, el);
         }
     })
-
-    console.log(bandCopy);
-
+    //console.log(bandsCopy);
 }
 
 function removeArticle(el) {
@@ -38,6 +34,36 @@ function removeArticle(el) {
         }
     }
     return el.join('');
+}
+
+
+
+function findAndPlaceEl(arr, el, realEl) {
+   // console.log(arr, el)
+    for(let i = 0; i < arr.length; i++) {
+        //Gets array item without article
+        let tmpArrEl = removeArticle(arr[i]);
+        //console.log(el);
+        for(let j = 0; j < el.length; j++) {
+            //console.log(j)
+            if(tmpArrEl[j]) {
+                getLowestCharCode(tmpArrEl[j], el[j]);
+            } 
+        }
+    }
+}
+
+function getLowestCharCode(curr, newest) {
+    //console.log(curr, newest)
+    if(newest.toLowerCase().charCodeAt(0) < curr.toLowerCase().charCodeAt(0)) {
+        console.log('its lower')
+    }
+    else if(newest.toLowerCase().charCodeAt(0) == curr.toLowerCase().charCodeAt(0)) {
+        console.log('its the same')
+    } else {
+        console.log('it\'s higher')
+    }
+    // tmpArrEl.toLowerCase().charCodeAt(i)
 }
 
 (function initApp() {
