9de55b1225d836323f17361b400b0d061d4c3c46
Mark Fitzpatrick <markfitz815@gmail.com>
2018-11-21 15:58:58 +0000

    nav swaps position depending on scrollY

--- 14. Sticky Nav/scripts.js
+++ 14. Sticky Nav/scripts.js
@@ -1,10 +1,14 @@
 
 const navBar = document.querySelector('#main');
+const navLoc = navBar.offsetTop;
+let navFixed = false;
 
 
 function checkScroll(e) {
-    if(window.scrollY > navBar.offsetTop) {
-        console.log('yup');
+    if(((window.scrollY > navLoc) && !navFixed) || ((window.scrollY <= navLoc) && navFixed)) {
+        navBar.classList.toggle('relativeNav');
+        navBar.classList.toggle('fixedNav');
+        navFixed = !navFixed;
     }
 }
 
