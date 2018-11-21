8f32fe3b157731e9a120e92f796035edfa679009
Mark Fitzpatrick <markfitz815@gmail.com>
2018-11-21 18:31:32 +0000

    change animation to transition to make effect work multiple times

--- 14. Sticky Nav/scripts.js
+++ 14. Sticky Nav/scripts.js
@@ -2,12 +2,22 @@
 const navBar = document.querySelector('#main');
 const navLoc = navBar.offsetTop;
 let navFixed = false;
+let notFirstScroll = false;
 
 
 function checkScroll(e) {
-    if(((window.scrollY > navLoc) && !navFixed) || ((window.scrollY <= navLoc) && navFixed)) {
+    if((((window.scrollY > navLoc) && !navFixed) || ((window.scrollY <= navLoc) && navFixed)) && !notFirstScroll) {
         navBar.classList.toggle('relativeNav');
         navBar.classList.toggle('fixedNav');
+        document.querySelector('.logo').classList.remove('logoHide');
+        document.querySelector('.logo').classList.toggle('logoShow');
+        navFixed = !navFixed;
+        notFirstScroll = true;
+    } else if(((window.scrollY > navLoc) && !navFixed) || ((window.scrollY <= navLoc) && navFixed)) {
+        navBar.classList.toggle('relativeNav');
+        navBar.classList.toggle('fixedNav');
+        document.querySelector('.logo').classList.toggle('logoHideAn');
+        document.querySelector('.logo').classList.toggle('logoShow');
         navFixed = !navFixed;
     }
 }
