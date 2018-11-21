4fa9d42406a3f1d98193b8c83cbb229641407052
Mark Fitzpatrick <markfitz815@gmail.com>
2018-11-21 13:58:32 +0000

    log total hours and minutes

--- 13.TallyAndReduce/scripts.js
+++ 13.TallyAndReduce/scripts.js
@@ -1,15 +1,17 @@
 
 const timeItems = document.querySelectorAll('li');
 const timeArr = [];
+let totalMilliSeconds = 0;
 
+//Gets time data from nodelist
 function compileArray(arr, pushArr) {
     arr.forEach(el => {
         pushArr.push(el.attributes['data-time'].value)
     });
 };
 
+//converts time strings into array with hours/minutes/seconds
 function convertToNums(arr){
-    console.log(arr);
     arr.map((el, ind) => {
         let tmpEl = [];
         let splitEl = el.split('');
@@ -26,11 +28,38 @@ function convertToNums(arr){
                 tmpAdd += splitEl[i];
             }
         }
-        console.log(tmpEl);
+        timeArr[ind] = tmpEl;
     })
 };
+
+//converts all times to seconds
+function tallyNums(timeArr) {
+    
+    timeArr.map((el) =>{
+        for(let i = el.length-1; i >= 0; i--) {
+            if(i == el.length-1) {
+                totalMilliSeconds += parseInt(el[i]);
+            } else if(i == el.length-2) {
+                totalMilliSeconds += parseInt(el[i] * 60);
+            } else {
+                totalMilliSeconds += parseInt((el[i] * 60) * 60);
+            }
+        }
+    })
+    
+    totalMilliSeconds *= 1000;
+    console.log(totalMilliSeconds);
+}
+
+function getTime(t) {
+    const d = new Date(t);
+    console.log(d.getSeconds());
+    console.log(`${d.getHours()} : ${d.getMinutes()} : ${d.getSeconds()}`);
+}
 
 (function initApp() {
     compileArray(timeItems, timeArr);
     convertToNums(timeArr);
+    tallyNums(timeArr);
+    getTime(totalMilliSeconds);
 })();
