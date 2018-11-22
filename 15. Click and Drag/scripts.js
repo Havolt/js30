66338a150fd65b41635986eaad31c77a9d1ecc6c
Mark Fitzpatrick <markfitz815@gmail.com>
2018-11-22 14:32:17 +0000

    add click and drag feature

--- 15. Click and Drag/scripts.js
+++ 15. Click and Drag/scripts.js
@@ -1,18 +1,43 @@
-console.log('hi');
+
+
+
+const items = document.querySelector('.items');
+console.log(items)
 
 let mouseIsDown = false;
 
+let prevMouseX;
+let currScrollLeft = 0;
+
+function scrollItems(prevM, currM) {
+    const diffM = currM - prevM;
+    console.log(diffM + currScrollLeft);
+    currScrollLeft -= diffM;
+    console.log(currScrollLeft);
+    if(currScrollLeft < 0) {
+        currScrollLeft = 0;
+    }
+    else if (currScrollLeft > items.scrollWidth - window.innerWidth) {
+        currScrollLeft = items.scrollWidth - window.innerWidth;
+    }
+
+    items.scrollTo(currScrollLeft, 0);
+}
 
 (function initApp() {
-    document.querySelector('.items').addEventListener('mousedown', (e) => {
+    items.addEventListener('mousedown', (e) => {
         mouseIsDown = true;
     })
-    document.querySelector('.items').addEventListener('mouseup', (e) => {
+    items.addEventListener('mouseup', (e) => {
         mouseIsDown = false;
+        prevMouseX = 0;
     })
-    document.querySelector('.items').addEventListener('mousemove', (e) => {
+    items.addEventListener('mousemove', (e) => {
         if(mouseIsDown) {
-            console.log(e);
+            if(prevMouseX) {
+                scrollItems(prevMouseX, e.clientX);
+            }
+            prevMouseX = e.clientX;
         }
     })
 })();
