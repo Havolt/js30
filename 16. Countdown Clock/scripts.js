6aa50a5abda1271d5cd7a02aff10723bf28c9a3c
Mark Fitzpatrick <markfitz815@gmail.com>
2018-11-26 18:33:19 +0000

    add end time section

--- 16. Countdown Clock/scripts.js
+++ 16. Countdown Clock/scripts.js
@@ -14,16 +14,26 @@ function getCurrentTime() {
     }, 1000);
 }
 
+function timeEngine() {
+
+}
+
 function setTime(seconds) {
     startTime = currTime;
     endTime = currTime + (seconds * 1000);
     timeLeft = (endTime - startTime) / 1000;
+    showDispEnd(endTime);
     console.log(timeLeft)
 };
 
+function showDispEnd(time) {
+    let endMinutes = new Date(time).getMinutes();
+    let endHours = new Date(time).getHours();
+    document.querySelector('.display__end-time').innerHTML = `Be back at ${endHours}:${endMinutes}`;
+}
+
 (function initApp() {
     for(let i = 0; i < document.querySelectorAll('.timer__button').length; i++) {
-        
         document.querySelectorAll('.timer__button')[i].addEventListener('click', (e) => {
             setTime(e.target.attributes['data-time'].value);
         })
