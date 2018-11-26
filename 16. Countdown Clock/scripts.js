44439ddf5c14c38e0fbb86df50de4127651e6e98
Mark Fitzpatrick <markfitz815@gmail.com>
2018-11-26 18:17:45 +0000

    add time captures

--- 16. Countdown Clock/scripts.js
+++ 16. Countdown Clock/scripts.js
@@ -2,12 +2,23 @@
 const timeDisp = document.querySelector('.display__time-left');
 const timeEnd = document.querySelector('.display__end-time');
 
+let currTime;
 let startTime;
 let endTime;
+let timeLeft;
+
+function getCurrentTime() {
+    currTime = new Date().getTime();
+    setTimeout(() => {
+        getCurrentTime();
+    }, 1000);
+}
 
 function setTime(seconds) {
-    startTime = new Date();
-    console.log(seconds);
+    startTime = currTime;
+    endTime = currTime + (seconds * 1000);
+    timeLeft = (endTime - startTime) / 1000;
+    console.log(timeLeft)
 };
 
 (function initApp() {
@@ -17,4 +28,5 @@ function setTime(seconds) {
             setTime(e.target.attributes['data-time'].value);
         })
     }
+    getCurrentTime(currTime);
 })();
