8a205399661a24cacdf3cd1849c7fdf1397c23fc
Mark Fitzpatrick <markfitz815@gmail.com>
2018-11-26 18:42:03 +0000

    add time left countdown

--- 16. Countdown Clock/scripts.js
+++ 16. Countdown Clock/scripts.js
@@ -6,24 +6,33 @@ let currTime;
 let startTime;
 let endTime;
 let timeLeft;
+let inProgress = false;
 
 function getCurrentTime() {
     currTime = new Date().getTime();
     setTimeout(() => {
+        if(inProgress) {
+            showDispLeft(timeLeft);
+        }
         getCurrentTime();
     }, 1000);
 }
 
-function timeEngine() {
-
+function showDispLeft(time) {
+    timeLeft -= 1000;
+    if (timeLeft <= 0) {
+        inProgress = false;
+    }
+    let leftMinutes = new Date(timeLeft).getMinutes();
+    let leftSeconds = new Date(timeLeft).getSeconds();
+    document.querySelector('.display__time-left').innerHTML = `${leftMinutes}:${leftSeconds}`;
 }
 
 function setTime(seconds) {
     startTime = currTime;
     endTime = currTime + (seconds * 1000);
-    timeLeft = (endTime - startTime) / 1000;
+    timeLeft = endTime - startTime;
     showDispEnd(endTime);
-    console.log(timeLeft)
 };
 
 function showDispEnd(time) {
@@ -36,6 +45,7 @@ function showDispEnd(time) {
     for(let i = 0; i < document.querySelectorAll('.timer__button').length; i++) {
         document.querySelectorAll('.timer__button')[i].addEventListener('click', (e) => {
             setTime(e.target.attributes['data-time'].value);
+            inProgress = true;
         })
     }
     getCurrentTime(currTime);
