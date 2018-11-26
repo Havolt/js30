9960582d7ae7c3cb0e372b5878afeeb5ddf5e18f
Mark Fitzpatrick <markfitz815@gmail.com>
2018-11-26 19:13:53 +0000

    set up custom times through url

--- 16. Countdown Clock/scripts.js
+++ 16. Countdown Clock/scripts.js
@@ -10,10 +10,10 @@ let inProgress = false;
 
 function getCurrentTime() {
     currTime = new Date().getTime();
+    if(inProgress) {
+        showDispLeft(timeLeft);
+    }
     setTimeout(() => {
-        if(inProgress) {
-            showDispLeft(timeLeft);
-        }
         getCurrentTime();
     }, 1000);
 }
@@ -23,9 +23,17 @@ function showDispLeft(time) {
     if (timeLeft <= 0) {
         inProgress = false;
     }
+    let leftHours = new Date(timeLeft).getHours() - 1;
+    if(leftHours < 10) { leftHours = `0${leftHours}`};
     let leftMinutes = new Date(timeLeft).getMinutes();
+    if(leftMinutes < 10) { leftMinutes = `0${leftMinutes}`};
     let leftSeconds = new Date(timeLeft).getSeconds();
-    document.querySelector('.display__time-left').innerHTML = `${leftMinutes}:${leftSeconds}`;
+    if(leftSeconds < 10) { leftSeconds = `0${leftSeconds}`};
+    if(leftHours > 0) {
+        document.querySelector('.display__time-left').innerHTML = `${leftHours}:${leftMinutes}:${leftSeconds}`;
+    } else {
+        document.querySelector('.display__time-left').innerHTML = `${leftMinutes}:${leftSeconds}`;
+    }
 }
 
 function setTime(seconds) {
@@ -33,6 +41,7 @@ function setTime(seconds) {
     endTime = currTime + (seconds * 1000);
     timeLeft = endTime - startTime;
     showDispEnd(endTime);
+    showDispLeft(timeLeft);
 };
 
 function showDispEnd(time) {
@@ -41,12 +50,25 @@ function showDispEnd(time) {
     document.querySelector('.display__end-time').innerHTML = `Be back at ${endHours}:${endMinutes}`;
 }
 
+function customTime() {
+    let custTime = window.location.search.split('').splice(9).join('');
+    if(!isNaN(custTime)) {
+        custTime = parseInt(custTime) ;
+        custTime *= 60;
+        inProgress = true;
+        setTime(custTime);
+    }
+    
+    
+}
+
 (function initApp() {
     for(let i = 0; i < document.querySelectorAll('.timer__button').length; i++) {
         document.querySelectorAll('.timer__button')[i].addEventListener('click', (e) => {
-            setTime(e.target.attributes['data-time'].value);
             inProgress = true;
+            setTime(e.target.attributes['data-time'].value);
         })
     }
     getCurrentTime(currTime);
+    customTime();
 })();
